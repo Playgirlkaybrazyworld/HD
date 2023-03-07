2557ddf4aab9abb1a271ace46e83fbb595627600
Jack Palevich <jack.palevich@gmail.com>
2023-03-06 21:02:42 -0800

    Initial Commit

---
+++ HD/HDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDApp.swift
+//  HD
+//
+//  Created by Jack Palevich on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
