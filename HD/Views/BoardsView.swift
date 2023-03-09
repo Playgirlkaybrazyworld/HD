2093be04cd84387b71846efd04163c21948a8084
Jack Palevich <jack.palevich@gmail.com>
2023-03-08 21:04:53 -0800

    Trying to get navigation to work nicely in detail view.

--- HD/Views/BoardsView.swift
+++ HD/Views/BoardsView.swift
@@ -13,18 +13,18 @@ struct BoardsView: View {
   @EnvironmentObject private var client: Client
   @State private var boards: [Board] = []
   @State private var selectedBoard: Board?
-
+  
   var body: some View {
     NavigationSplitView(sidebar: { sidebar }, detail: { detail } )
-    .onAppear {
-      Task {
-        let boards: Boards = try await client.get(endpoint: .boards)
-        withAnimation {
-          self.boards = boards.boards
+      .onAppear {
+        Task {
+          let boards: Boards = try await client.get(endpoint: .boards)
+          withAnimation {
+            self.boards = boards.boards
+          }
         }
       }
-    }
-    .navigationTitle("boards")
+      .navigationTitle("boards")
   }
   
   @ViewBuilder
@@ -38,12 +38,14 @@ struct BoardsView: View {
   
   @ViewBuilder
   var detail: some View {
-    if let selectedBoard = selectedBoard {
-      CatalogView(board:selectedBoard)
-        // ID is required to force the detail to refresh on iOS 16.2 simulator.
-        .id(selectedBoard.id)
-    } else {
-      Text("Select a board from the sidebar.")
+    NavigationStack {
+      if let selectedBoard = selectedBoard {
+        CatalogView(board:selectedBoard)
+        // ID forces the detail to refresh on iOS 16.2 simulator.
+          .id(selectedBoard.id)
+      } else {
+        Text("Select a board from the sidebar.")
+      }
     }
   }
 }
