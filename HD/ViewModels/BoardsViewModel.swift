d01c8f4be75e0cc17d17a0e58af04c5a623a3c53
Jack Palevich <jack.palevich@gmail.com>
2023-04-02 07:05:54 -0700

    Remove unused part of view models.

--- HD/ViewModels/BoardsViewModel.swift
+++ HD/ViewModels/BoardsViewModel.swift
@@ -5,21 +5,8 @@ class BoardsViewModel : ObservableObject {
   @Published var scrollToBoard: String?
   @Published var boardsState: BoardsState = .loading
   
-  var visibleBoards = Set<String>()
   var scrollToBoardAnimated: Bool = false
-  
-  func appeared(board: String) {
-    visibleBoards.insert(board)
-  }
-  
-  func disappeared(board: String) {
-    visibleBoards.remove(board)
-  }
-  
-  var topVisibleBoard: String? {
-    visibleBoards.sorted().first
-  }
-  
+    
   func index(board:String)->Int? {
     if case let .display(boards) = boardsState {
       return boards.firstIndex{ $0.id == board }
--- HD/ViewModels/CatalogViewModel.swift
+++ HD/ViewModels/CatalogViewModel.swift
@@ -5,21 +5,8 @@ class CatalogViewModel : ObservableObject {
   @Published var scrollToThread: PostNumber?
   @Published var catalogState: CatalogState = .loading
   
-  var visibleThreads = Set<PostNumber>()
   var scrollToThreadAnimated: Bool = false
-  
-  func appeared(thread: PostNumber) {
-    visibleThreads.insert(thread)
-  }
-  
-  func disappeared(thread: PostNumber) {
-    visibleThreads.remove(thread)
-  }
-  
-  var topVisibleThread: PostNumber? {
-    visibleThreads.sorted().first
-  }
-  
+    
   func index(thread:PostNumber)->Int? {
     if case let .display(threads) = catalogState {
       return threads.firstIndex{ $0.id == thread }
