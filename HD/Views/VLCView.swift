0b8636c8ae8aa3a017cc308602f2ea3447cb4a79
Jack Palevich <jack.palevich@gmail.com>
2023-04-03 22:47:41 -0700

    Add simple play/pause and mute/unmute controls.

--- HD/Views/VLCView.swift
+++ HD/Views/VLCView.swift
@@ -2,25 +2,72 @@ import SwiftUI
 import VLCKitSPM
 import UIKit
 
+struct PlayButton: View {
+  @Binding var isPlaying: Bool
+  
+  var body: some View {
+    Button{
+      isPlaying.toggle()
+    } label: {
+      Image(systemName: isPlaying ? "pause" : "play")
+    }
+  }
+}
+
+struct MuteButton: View {
+  @Binding var isMuted: Bool
+  
+  var body: some View {
+    Button{
+      isMuted.toggle()
+    } label: {
+      Image(systemName: isMuted ? "speaker" : "speaker.slash")
+    }
+  }
+}
+
+
 struct VLCView: View {
   let mediaURL: URL
   let width: Int
   let height: Int
   
+  @State private var isMuted: Bool = true
+  @State private var isPlaying: Bool = true
+  
   var body: some View {
-    VLCViewImpl(mediaURL: mediaURL, width: width, height: height)
+    video
+      .overlay(controls.padding(4), alignment:.bottomTrailing)
+  }
+  
+  var video: some View {
+    VLCViewImpl(mediaURL: mediaURL, width: width, height: height, isPlaying: isPlaying, isMuted: isMuted)
       .aspectRatio(CGSize(width:width, height:height), contentMode: .fit)
   }
-
+  
+  var controls: some View {
+    HStack{
+      PlayButton(isPlaying: $isPlaying)
+      MuteButton(isMuted: $isMuted)
+    }
+    .padding(4)
+    .background(
+      .thinMaterial,
+      in: RoundedRectangle(cornerRadius: 4)
+    )
+    
+  }
 }
 
 struct VLCViewImpl: UIViewControllerRepresentable {
   typealias UIViewControllerType = VLCViewController
   @Environment(\.scenePhase) private var scenePhase
-
+  
   let mediaURL: URL
   let width: Int
   let height: Int
+  let isPlaying: Bool
+  let isMuted: Bool
   
   func makeUIViewController(context: Context) -> VLCViewController {
     let vc = VLCViewController()
@@ -34,7 +81,18 @@ struct VLCViewImpl: UIViewControllerRepresentable {
     if let mediaListPlayer = uiViewController.mediaListPlayer,
        let view = uiViewController.view {
       if scenePhase == .active {
-        mediaListPlayer.play()
+        if let vlcAudio = mediaListPlayer.mediaPlayer.audio {
+          let isMuted = vlcAudio.volume == 0
+          if isMuted != self.isMuted {
+            mediaListPlayer.pause()
+            vlcAudio.volume = Int32(self.isMuted ? 0 : 100)
+          }
+        }
+        if isPlaying {
+          mediaListPlayer.play()
+        } else {
+          mediaListPlayer.pause()
+        }
         view.alpha = 1.0
       } else {
         mediaListPlayer.pause()
@@ -51,9 +109,9 @@ class VLCViewController: UIViewController, VLCMediaListPlayerDelegate {
   var mediaURL: URL!
   var width: Int = 0
   var height: Int = 0
-
+  
   var mediaListPlayer: VLCMediaListPlayer!
-
+  
   override func loadView() {
     view = UIView()
   }
@@ -68,10 +126,10 @@ class VLCViewController: UIViewController, VLCMediaListPlayerDelegate {
     let mediaList = VLCMediaList()
     mediaList.add(media)
     mediaListPlayer.mediaList = mediaList
-    mediaListPlayer.play(media)
     if let vlcAudio = mediaListPlayer.mediaPlayer.audio {
       vlcAudio.volume = 0
     }
+    mediaListPlayer.play(media)
   }
   
   override func viewWillDisappear(_ animated: Bool) {
